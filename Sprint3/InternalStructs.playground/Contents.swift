e7c7c4c722a2c488de7d3e61415fa39cb273cf3d
Femin Dharamshi <femin.dharamshi@sakec.ac.in>
2022-10-26 17:05:35 -0400

    TODOs added on playground for structs

--- Sprint3/InternalStructs.playground/Contents.swift
+++ Sprint3/InternalStructs.playground/Contents.swift
@@ -3,6 +3,11 @@ import UIKit
 /** This playground contains the structs for the internal API that will be created in Sprint4
  Using Django Framwork. **/
 
+/* NOTE: Every response will contain a success boolean which will denote if the process was successful or not.
+   It will also contain a message String, which can be used to send a message back to the mobile app:
+   incase there was an error, we can give more details of the error
+   Or else it will just contain "Success!" */
+
 // ### STRUCT FOR SIGNUP REQUEST ###
 struct SignUp: Decodable {
   let success: Bool
@@ -38,6 +43,8 @@ struct TravelPoint : Decodable {
   let zip: String
   let city: String
   let date: String
+  let userID: Int
+  let UserProfilePic: String
   
   enum CodingKeys : String, CodingKey {
     case lat
@@ -45,6 +52,8 @@ struct TravelPoint : Decodable {
     case zip
     case city
     case date
+    case userID = "user_id"
+    case UserProfilePic = "profile_url"
   }
 }
 
@@ -95,7 +104,7 @@ struct Book : Decodable {
 // TODO: API for Search
 
 
-// ### STRUCTS FOR BOOK/COPY INFORMATION PAGE ###
+// ### STRUCTS FOR BOOK INFORMATION PAGE ###
 struct CommentsBookDetails : Decodable {
   
   // TODO: Complete this: Name, Comment, Date
@@ -110,6 +119,21 @@ struct AvailableCopies : Decodable {
 
 struct BookDetails : Decodable {
   
-  // TODO: Implement this: BookID, Title, Author, Rating, Description?, CoverURL, [CommentsBookDetails], [AvailableCopies]
+  // TODO: Complete this: BookID, Title, Author, Rating, Description?, CoverURL, [CommentsBookDetails], [AvailableCopies]
   
 }
+
+// ### STRUCTS FOR COPY INFORMATION PAGE ###
+
+struct CopyDetails : Decodable {
+  
+  // TODO: Complete this: copyID, status, [TravelPoint], title, author, coverURL, rating, shippingExpense, Willingness To Ship, Book Condition, Notes
+  
+}
+
+struct RequestResult : Decodable {
+  
+  // TODO: Complete this: success, msg, requestID
+  
+}
+
