659c2cf97e8d6106250a94f0f480f8189c5bd4ab
Femin Dharamshi <femin.dharamshi@sakec.ac.in>
2022-10-26 16:53:24 -0400

    Playground for Structs created in Sprint3

---
+++ Sprint3/InternalStructs.playground/Contents.swift
@@ -0,0 +1,97 @@
+import UIKit
+
+/** This playground contains the structs for the internal API that will be created in Sprint4
+ Using Django Framwork. **/
+
+// ### STRUCT FOR SIGNUP REQUEST ###
+struct SignUp: Decodable {
+  let success: Bool
+  let msg: String
+  
+  let userId: Int
+  
+  enum CodingKeys : String, CodingKey {
+    case success
+    case msg
+    case userId = "user_id"
+  }
+}
+
+// ### STRUCT FOR LOGIN REQUEST ###
+struct LogIn: Decodable {
+  let success: Bool
+  let msg: String
+  
+  let userId: Int
+  
+  enum CodingKeys : String, CodingKey {
+    case success
+    case msg
+    case userId = "user_id"
+  }
+}
+
+// ### STRUCTS FOR MAIN PAGE ###
+struct TravelPoint : Decodable {
+  let lat: Float
+  let lon: Float
+  let zip: String
+  let city: String
+  let date: String
+  
+  enum CodingKeys : String, CodingKey {
+    case lat
+    case lon
+    case zip
+    case city
+    case date
+  }
+}
+
+
+struct OurPick: Decodable {
+  let success: Bool
+  let msg: String
+  
+  let copyId: Int
+  let bookId: Int
+  let userId: Int
+  let travelHistory: [TravelPoint]
+  
+  enum CodingKeys : String, CodingKey {
+    case success
+    case msg
+    
+    case copyId = "copy_id"
+    case bookId = "boook_id"
+    case userId = "user_id"
+    case travelHistory = "travel_history"
+  }
+}
+
+struct Book : Decodable {
+  
+  let success: Bool
+  let msg: String
+  
+  let bookID: Int
+  let coverURL: String
+  let title: String
+  let author: String
+  let rating: Int
+  
+  enum CodingKeys : String, CodingKey {
+    case success
+    case msg
+    
+    case bookID = "boook_id"
+    case coverURL = "cover_url"
+    case title
+    case author
+    case rating
+  }
+}
+
+// TODO: API for Search
+
+
