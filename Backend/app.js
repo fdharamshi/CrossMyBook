aa9048d8a7b1afc6ca2b8899fe3aee7c7a137ea8
Femin Dharamshi <femin.dharamshi@sakec.ac.in>
2022-10-25 22:01:26 -0400

    API Skeleton added

--- Backend/app.js
+++
@@ -1,8 +0,0 @@
-const express = require('express')
-const app = express();
-
-app.get('/', (req, res) => {
-    res.send("Working");
-});
-
-app.listen(3000, () => console.log("App is listening on Port 3000"))
