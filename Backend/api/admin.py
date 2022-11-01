15843883e39487d8a6d9d80206fe8b7364ccca98
Femin Dharamshi <femin.dharamshi@sakec.ac.in>
2022-10-31 22:25:39 -0400

    Models registered in Django Admin

--- Backend/api/admin.py
+++ Backend/api/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from api.models import *
 
 # Register your models here.
+admin.site.register(Users)
+admin.site.register(Book)
+admin.site.register(BookCopy)
+admin.site.register(Listing)
+admin.site.register(Request)
+admin.site.register(Review)
+admin.site.register(TravelHistory)
