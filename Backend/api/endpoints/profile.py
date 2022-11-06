cb5c23d26e76dff3e0324373fc48c9b91cd25234
Femin Dharamshi <femin.dharamshi@sakec.ac.in>
2022-11-06 00:50:11 -0400

    Backend fixed

--- Backend/api/endpoints/profile.py
+++ Backend/api/endpoints/profile.py
@@ -1,8 +1,8 @@
 from django.http import JsonResponse
 from api.models import *
 
-def getProfile(request):
 
+def getProfile(request):
     # This is a GET request
     user_id = request.GET.get("user_id", None)
 
@@ -15,19 +15,15 @@ def getProfile(request):
     except Users.DoesNotExist:
         return JsonResponse({'msg': 'User not logged in.', 'success': False}, safe=False)
 
-
-    review_num = Reviews.objects.filter(user=user).count()
+    review_num = Review.objects.filter(user=user).count()
     response = {
         "success": True,
         "msg": "Success!",
-        # TODO: Still need:
         "first_name": user.first_name,
         "last_name": user.last_name,
         "email": user.email,
         "user_id": user.id,
         "profile_url": user.profile_url,
         "review_number": review_num
-
     }
     return JsonResponse(response, safe=False)
-
--- Backend/api/endpoints/releases.py
+++ Backend/api/endpoints/releases.py
@@ -1,4 +1,4 @@
-#from symbol import except_clause
+# from symbol import except_clause
 from django.http import JsonResponse
 import requests
 from api.models import *
@@ -13,32 +13,36 @@ def getDetailsFromISBN(request):
     # Try to find ISBN in DB
     try:
         book = Book.objects.get(isbn=isbn)
-        return JsonResponse({"isbn": book.isbn, "title": book.title, "cover_url": book.cover_url, "author": book.authors, "book_id": book.id, "rating": 5}, safe=False)
-    
+        return JsonResponse({"isbn": book.isbn, "title": book.title, "cover_url": book.cover_url,
+                             "author": book.authors, "book_id": book.id, "rating": 5}, safe=False)
+
     except Book.DoesNotExist:
         # FETCH from OpenLibrary
 
         try:
-            response = requests.get("https://openlibrary.org/isbn/"+isbn+".json")
+            response = requests.get("https://openlibrary.org/isbn/" + isbn + ".json")
             response = response.json()
 
             authors = ""
             # Iteratively fetch authors from openlibrary
             for author in response["authors"]:
-                authorResponse = requests.get("https://openlibrary.org"+author["key"]+".json")
+                authorResponse = requests.get("https://openlibrary.org" + author["key"] + ".json")
                 authors = authors + authorResponse.json()["name"] + ", "
-            authors=authors[:-2]
+            authors = authors[:-2]
 
-            book = Book(title=response['title'], isbn=isbn, cover_url="https://covers.openlibrary.org/b/id/"+str(response["covers"][0])+"-L.jpg", authors=authors)
+            book = Book(title=response['title'], isbn=isbn,
+                        cover_url="https://covers.openlibrary.org/b/id/" + str(response["covers"][0]) + "-L.jpg",
+                        authors=authors)
             book.save()
-            return JsonResponse({"isbn": book.isbn, "title": book.title, "cover_url": book.cover_url, "author": book.authors, "book_id": book.id, "rating": 5}, safe=False)
-        
+            return JsonResponse(
+                {"isbn": book.isbn, "title": book.title, "cover_url": book.cover_url, "author": book.authors,
+                 "book_id": book.id, "rating": 5}, safe=False)
+
         except:
             return JsonResponse({'msg': 'Book not found.', 'success': False}, safe=False)
 
 
 def releaseANewCopy(request):
-
     # THIS IS A POST REQUEST SINCE A NEW RECORD WILL BE CREATED.
 
     user_id = request.POST.get("user_id", None)
@@ -64,7 +68,8 @@ def releaseANewCopy(request):
 
     # CREATE A NEW LISTING
     try:
-        new_listing = Listing(copy=copy, user_id=user_id, lat=lat, lon=lon, book_condition=book_condition, charges=charges, max_distance=max_distance, note=note, status=status)
+        new_listing = Listing(copy=copy, user_id=user_id, lat=lat, lon=lon, book_condition=book_condition,
+                              charges=charges, max_distance=max_distance, note=note, status=status)
         new_listing.save()
     except:
         return JsonResponse({'msg': 'Fail to create a new listing.', 'success': False}, safe=False)
@@ -75,11 +80,11 @@ def releaseANewCopy(request):
     except:
         return JsonResponse({'msg': 'Fail to create a new travel point.', 'success': False}, safe=False)
 
-    return JsonResponse({'msg': 'Success!', 'success': True, "listing_id": new_listing.id, "copy_id": copy.id}, safe=False)
+    return JsonResponse({'msg': 'Success!', 'success': True, "listing_id": new_listing.id, "copy_id": copy.id},
+                        safe=False)
 
 
 def releaseACopyAlreadyCreated(request):
-
     user_id = request.POST.get("user_id", None)
     copy_id = request.POST.get("book_id", None)
 
@@ -102,8 +107,9 @@ def releaseACopyAlreadyCreated(request):
 
     # CREATE A NEW LISTING
     try:
-        new_listing = Listing(copy=copy, user_id=user_id, lat=lat, long=lon, book_condition=book_condition, charges=charges,
-                            max_distance=max_distance, note=note, status=status)
+        new_listing = Listing(copy=copy, user_id=user_id, lat=lat, long=lon, book_condition=book_condition,
+                              charges=charges,
+                              max_distance=max_distance, note=note, status=status)
         new_listing.save()
     except:
         return JsonResponse({'msg': 'Fail to create a new listing.', 'success': False}, safe=False)
