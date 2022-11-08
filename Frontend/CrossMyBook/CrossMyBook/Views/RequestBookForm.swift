c65d33f87e276eda5bf003de1691de810d6619b5
eutinawei <46619191+etwyt@users.noreply.github.com>
2022-11-07 23:56:20 -0500

    Add reusable textfield

--- Frontend/CrossMyBook/CrossMyBook/Views/RequestBookForm.swift
+++ Frontend/CrossMyBook/CrossMyBook/Views/RequestBookForm.swift
@@ -57,27 +57,14 @@ struct RequestBookForm: View {
           Spacer()
         }
         VStack {
-          RoundedRectangle(cornerRadius: 10)
-            .fill(.white)
-            .overlay(
-              TextField("name", text: .constant("Femin Dharamshi"))
-                .padding(.leading)
-                .font(Font.custom("NotoSerif", size: 14))
-            )
-            .frame(height: 48)
+
+          
+          RoundedTextField(text: .constant("Femin Dharamshi"), placeholder: "name", height: 48)
           
           Button("Fetch Current Location", action: {})
           Text("Lat: 123.123 Lon: -79.123")
           
-          RoundedRectangle(cornerRadius: 10)
-            .fill(.white)
-            .overlay(
-              TextField("Leave a note", text: $note, axis: .vertical)
-                .padding(.leading)
-                .multilineTextAlignment(.leading)
-                .font(Font.custom("NotoSerif", size: 14))
-            )
-            .frame(height: 250)
+          RoundedTextField(text: $note, placeholder: "leave a note", height: 250)
           
         }.padding(EdgeInsets(top: 20, leading: 20, bottom: 0, trailing: 20))
       }
---
+++ Frontend/CrossMyBook/CrossMyBook/Views/RoundedTextField.swift
@@ -0,0 +1,25 @@
+//
+//  RoundedTextField.swift
+//  CrossMyBook
+//
+//  Created by 魏妤庭 on 2022/11/7.
+//
+
+import SwiftUI
+
+struct RoundedTextField: View {
+    @Binding var text: String
+    var placeholder: String
+    var height: CGFloat
+  
+    var body: some View {
+        RoundedRectangle(cornerRadius: 10)
+          .fill(.white)
+          .overlay(
+            TextField(placeholder, text: $text)
+              .padding(.leading)
+              .font(Font.custom("NotoSerif", size: 14))
+          )
+          .frame(height: height)
+    }
+}
