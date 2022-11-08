438cbab4eb40395bc408ec24b16b21d249939871
Femin Dharamshi <femin.dharamshi@sakec.ac.in>
2022-11-08 18:54:20 -0500

    temporary endpoint fixed

--- Backend/api/endpoints/mainpage.py
+++ Backend/api/endpoints/mainpage.py
@@ -3,11 +3,11 @@ from api.models import *
 
 
 def temporary_main(request):
-    all_listings = []
+    all_copies = []
     all_books = []
 
     books = Book.objects.all()
-    listings = Listing.objects.all()
+    copies = BookCopy.objects.all()
 
     for book in books:
         all_books.append({
@@ -17,14 +17,13 @@ def temporary_main(request):
             "author": book.authors
         })
 
-    for listing in listings:
-        all_listings.append({
-            "listing_id": listing.id,
-            "status": listing.status,
-            "title": listing.copy.book.title,
-            "cover_url": listing.copy.book.cover_url,
-            "author": listing.copy.book.authors
+    for copy in copies:
+        all_copies.append({
+            "listing_id": copy.id,
+            "title": copy.book.title,
+            "cover_url": copy.book.cover_url,
+            "author": copy.book.authors
         })
 
-    return JsonResponse({'msg': 'Success!', 'success': True, "all_books": all_books, "all_listings": all_listings},
+    return JsonResponse({'msg': 'Success!', 'success': True, "all_books": all_books, "all_copies": all_copies},
                         safe=False)
