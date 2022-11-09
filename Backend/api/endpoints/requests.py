c1808735acb6176e78811baf137451cb0629e2ef
Femin Dharamshi <femin.dharamshi@sakec.ac.in>
2022-11-08 22:10:02 -0500

    Get copy details rating fixed

--- Backend/api/endpoints/requests.py
+++ Backend/api/endpoints/requests.py
@@ -21,7 +21,7 @@ def get_copy_details(request):
 
     # get average of all reviews
     rating = {
-        "avg__starts": 5
+        "avg__stars": 5
     }
     reviews = Review.objects.filter(book=copy.book)
     if reviews.count() > 0:
