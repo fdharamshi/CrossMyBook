01ca704c7bae6b42e3c30f71be2595fd237b8740
Femin Dharamshi <femin.dharamshi@sakec.ac.in>
2022-11-10 10:59:56 -0500

    User Defaults added

---
+++ Frontend/MLClassExercise/MLClassExercise/MLClassExerciseApp.swift
@@ -0,0 +1,17 @@
+//
+//  MLClassExerciseApp.swift
+//  MLClassExercise
+//
+//  Created by Femin Dharamshi on 11/10/22.
+//
+
+import SwiftUI
+
+@main
+struct MLClassExerciseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
