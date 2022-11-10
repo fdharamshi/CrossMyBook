eafe01112334e0cbaab1cd45b1c7b308e8de821f
Femin Dharamshi <femin.dharamshi@sakec.ac.in>
2022-11-10 11:43:47 -0500

    User Defaults added

--- Frontend/CrossMyBook/CrossMyBook/Controllers/CopyDetailsController/CopyDetailsController.swift
+++ Frontend/CrossMyBook/CrossMyBook/Controllers/CopyDetailsController/CopyDetailsController.swift
@@ -15,8 +15,8 @@ class CopyDetailsController: ObservableObject {
     self.observedCopy = nil
   }
   
-  func fetchCopyDetails(_ copyID: Int) {
-    fetchData(copyID, 2, completion: { copyDetailsModel in
+  func fetchCopyDetails(_ copyID: Int, _ userID: Int) {
+    fetchData(copyID, userID, completion: { copyDetailsModel in
       self.observedCopy = copyDetailsModel
     })
   }
