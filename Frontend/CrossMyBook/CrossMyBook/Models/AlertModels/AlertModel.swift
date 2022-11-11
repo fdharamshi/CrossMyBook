c1ccdb67c02a5a7a8ce9560d812eb673e5c5ba89
Femin Dharamshi <femin.dharamshi@sakec.ac.in>
2022-11-11 14:30:07 -0500

    Distance added on alert view

--- Frontend/CrossMyBook/CrossMyBook/Models/AlertModels/AlertModel.swift
+++ Frontend/CrossMyBook/CrossMyBook/Models/AlertModels/AlertModel.swift
@@ -9,39 +9,48 @@ import Foundation
 
 // MARK: - Welcome
 struct AlertModel: Codable {
-    let msg: String
-    let success: Bool
-    let pendingRequests, acceptedRequests: [Request]
-
-    enum CodingKeys: String, CodingKey {
-        case msg, success
-        case pendingRequests = "pending_requests"
-        case acceptedRequests = "accepted_requests"
-    }
+  let msg: String
+  let success: Bool
+  let pendingRequests, acceptedRequests: [Request]
+  
+  enum CodingKeys: String, CodingKey {
+    case msg, success
+    case pendingRequests = "pending_requests"
+    case acceptedRequests = "accepted_requests"
+  }
 }
 
 // MARK: - Request
 struct Request: Codable, Identifiable {
-    let id = UUID()
-    let userID: Int
-    let userName: String
-    let copyID, listingID: Int
-    let date: String
-    let lat, lon: Double
-    let note: String
-    let requestID: Int
-    let coverURL: String
-    let title: String
+  let id = UUID()
+  let userID: Int
+  let userProfile: String
+  let userName: String
+  let copyID, listingID: Int
+  let date: String
+  let lat, lon: Double
+  let note: String
+  let requestID: Int
+  let coverURL: String
+  let title: String
+  let listingLOC: ListingLOC
+  
+  enum CodingKeys: String, CodingKey {
+    case userID = "user_id"
+    case userProfile = "user_profile"
+    case userName = "user_name"
+    case copyID = "copy_id"
+    case listingID = "listing_id"
+    case date, lat, lon, note
+    case requestID = "request_id"
+    case coverURL = "cover_url"
+    case title
+    case listingLOC = "listing_loc"
+  }
+}
 
-    enum CodingKeys: String, CodingKey {
-        case userID = "user_id"
-        case userName = "user_name"
-        case copyID = "copy_id"
-        case listingID = "listing_id"
-        case date, lat, lon, note
-        case requestID = "request_id"
-        case coverURL = "cover_url"
-        case title
-    }
+// MARK: - ListingLOC
+struct ListingLOC: Codable {
+  let lat, lon: Double
 }
 
--- Frontend/CrossMyBook/CrossMyBook/Views/AlertView.swift
+++ Frontend/CrossMyBook/CrossMyBook/Views/AlertView.swift
@@ -7,6 +7,7 @@
 
 import SwiftUI
 import SDWebImageSwiftUI
+import MapKit
 
 struct AlertView: View {
   
@@ -26,19 +27,27 @@ struct AlertView: View {
     }
   }
   
+  func getDistance(_ lat1:Double, _ lon1:Double, _ lat2: Double, _ lon2: Double) -> String {
+    let userLoc:CLLocation = CLLocation(latitude: lat1, longitude: lon1)
+    let listingLoc:CLLocation = CLLocation(latitude: lat2, longitude: lon2)
+    
+    
+    return String(format: "%.2f", (listingLoc.distance(from: userLoc) / 1609))
+  }
+  
   var body: some View {
     VStack {
       
       // MARK: TOP BAR
-        ZStack (alignment: .leading) {
-          Text("Requests")
-            .font(.custom("NotoSerif", size: 25)).bold().frame(maxWidth: .infinity).foregroundColor(.fontBlack)
-          Button (action: {
-              self.presentationMode.wrappedValue.dismiss() // TODO: back action
-          }) {
-            FAIcon(name: "chevron-left", size: 25)
-          }.padding(.trailing, 20.0)
-        }.padding(10).background(Color(red: 245/255, green: 245 / 255, blue: 245 / 255))
+      ZStack (alignment: .leading) {
+        Text("Requests")
+          .font(.custom("NotoSerif", size: 25)).bold().frame(maxWidth: .infinity).foregroundColor(.fontBlack)
+        Button (action: {
+          self.presentationMode.wrappedValue.dismiss() // TODO: back action
+        }) {
+          FAIcon(name: "chevron-left", size: 25)
+        }.padding(.trailing, 20.0)
+      }.padding(10).background(Color(red: 245/255, green: 245 / 255, blue: 245 / 255))
       
       // MARK: TAB BAR
       VStack {
@@ -100,7 +109,7 @@ struct AlertView: View {
                 Text(request.title).font(.custom("NotoSerif", size: 15)).bold().frame(width: 80).lineLimit(4)
               }
               VStack (alignment: .leading) {
-                Text("Request ID: \(request.requestID)").padding(.bottom, 5.0)
+                Text("Distance: \(getDistance(request.lat, request.lon, request.listingLOC.lat, request.listingLOC.lon)) mi.").padding(.bottom, 5.0)
                 Text("Requester:").font(.custom("NotoSerif", size: 16)).bold()
                 Text("\(request.userName)").padding(.bottom, 5.0)
                 Text("Note:").font(.custom("NotoSerif", size: 16)).bold()
@@ -109,9 +118,9 @@ struct AlertView: View {
               Spacer()
             }.padding(.horizontal, 20.0)
             Divider()
-            }
           }
         }
+      }
       
       
     }.background(Color(red: 245/255, green: 245 / 255, blue: 245 / 255)).onAppear(perform: {
