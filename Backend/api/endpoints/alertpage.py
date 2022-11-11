a23f9ad1dc5aa023e971f3e88308a7e043374b27
Femin Dharamshi <femin.dharamshi@sakec.ac.in>
2022-11-11 14:14:37 -0500

    Alert Page updated

--- Backend/api/endpoints/alertpage.py
+++ Backend/api/endpoints/alertpage.py
@@ -3,7 +3,6 @@ from api.models import *
 
 
 def getRequests(request):
-
     # THIS IS A GET REQUEST.
 
     user_id = request.GET.get("user_id", None)
@@ -19,7 +18,8 @@ def getRequests(request):
 
     # FETCH PENDING REQUESTS FOR THE USER:
     try:
-        pending = Request.objects.filter(listing_id__in=Listing.objects.filter(user_id=user_id, status=0).values_list('id', flat=True), status=0)
+        pending = Request.objects.filter(
+            listing_id__in=Listing.objects.filter(user_id=user_id, status=0).values_list('id', flat=True), status=0)
         # My idea of it: Find all the listings this particular user has made. Then find all the requests for those listings
         # Which have status code 0
     except:
@@ -27,7 +27,8 @@ def getRequests(request):
 
     # FETCH ACCEPTED REQUESTS:
     try:
-        accepted = Request.objects.filter(listing_id__in=Listing.objects.filter(user_id=user_id, status=1).values_list('id', flat=True), status=1)
+        accepted = Request.objects.filter(
+            listing_id__in=Listing.objects.filter(user_id=user_id, status=1).values_list('id', flat=True), status=1)
     except:
         return JsonResponse({'msg': 'Fail to fetch acccepted requests.', 'success': False}, safe=False)
 
@@ -37,7 +38,8 @@ def getRequests(request):
     for r in pending:
         pending_requests.append({
             "user_id": r.requester.id,
-            "user_name": r.requester.first_name + r.requester.last_name,
+            "user_profile": r.requester.profile_url,
+            "user_name": r.requester.first_name + " " + r.requester.last_name,
             "copy_id": r.copy.id,
             "listing_id": r.listing.id,
             "date": r.date,
@@ -47,13 +49,18 @@ def getRequests(request):
             "request_id": r.id,
 
             "cover_url": r.copy.book.cover_url,
-            "title": r.copy.book.title
+            "title": r.copy.book.title,
+            "listing_loc": {
+                "lat": r.listing.lat,
+                "lon": r.listing.lon
+            }
         })
 
     for r in accepted:
         accepted_requests.append({
             "user_id": r.requester.id,
-            "user_name": r.requester.first_name + r.requester.last_name,
+            "user_profile": r.requester.profile_url,
+            "user_name": r.requester.first_name + " " + r.requester.last_name,
             "copy_id": r.copy.id,
             "listing_id": r.listing.id,
             "date": r.date,
@@ -63,8 +70,12 @@ def getRequests(request):
             "request_id": r.id,
 
             "cover_url": r.copy.book.cover_url,
-            "title": r.copy.book.title
+            "title": r.copy.book.title,
+            "listing_loc": {
+                "lat": r.listing.lat,
+                "lon": r.listing.lon
+            }
         })
 
-    return JsonResponse({'msg': 'Success!', 'success': True, "pending_requests": pending_requests, "accepted_requests": accepted_requests}, safe=False)
-
+    return JsonResponse({'msg': 'Success!', 'success': True, "pending_requests": pending_requests,
+                         "accepted_requests": accepted_requests}, safe=False)
