721b0ea12225c3d74ebb3a2a5cf27d76514ab0a9
Femin Dharamshi <femin.dharamshi@sakec.ac.in>
2022-11-11 15:10:36 -0500

    Request View created

--- Frontend/CrossMyBook/CrossMyBook/Views/AlertRequestView.swift
+++ Frontend/CrossMyBook/CrossMyBook/Views/AlertRequestView.swift
@@ -6,17 +6,93 @@
 //
 
 import SwiftUI
+import SDWebImageSwiftUI
+import MapKit
 
 struct AlertRequestView: View {
+  
+  @State var request: Request
+  
   var body: some View {
     VStack (alignment: .leading){
       
       ScrollView {
         VStack (alignment: .leading) {
           
-          HStack (alignment: .center) {
+          HStack (alignment: .top) {
+            WebImage(url: URL(string: request.coverURL))
+              .resizable()
+              .placeholder(Image(uiImage: UIImage(named: "bookplaceholder")!)) // Placeholder Image
+              .scaledToFit()
+              .frame(width: 100, height: 150, alignment: .center)
+              .border(Color.black, width: 1)
+              .background(Color.brown)
             
+            VStack (alignment: .leading) {
+              Text(request.title)
+                .frame(width: .infinity)
+                .multilineTextAlignment(.leading)
+                .fixedSize(horizontal: false, vertical: true)
+                .font(.custom("NotoSerif", size: 25))
+                .bold()
+              
+              Text("Author Here")
+                .font(.custom("NotoSerif", size: 15))
+                .multilineTextAlignment(.leading)
+                .fixedSize(horizontal: false, vertical: true)
+            }
           }
+          
+          Text("Requested By:")
+            .font(.custom("NotoSerif", size: 15)).bold()
+            .multilineTextAlignment(.leading)
+            .fixedSize(horizontal: false, vertical: true)
+            .padding(.top, 20.0)
+          
+          HStack (alignment: .center) {
+            Spacer()
+            VStack {
+              WebImage(url: URL(string: request.userProfile))
+                .resizable()
+                .scaledToFill()
+                .frame(width: 100, height: 100, alignment: .center)
+                .cornerRadius(50)
+                .padding(.trailing, 20.0)
+              Text(request.userName)
+                .frame(width: .infinity)
+                .multilineTextAlignment(.leading)
+                .fixedSize(horizontal: false, vertical: true)
+                .font(.custom("NotoSerif", size: 15))
+                .bold()
+            }
+            Spacer()
+          }
+          
+          HStack {
+            Text("Address: ")
+              .frame(width: .infinity)
+              .multilineTextAlignment(.leading)
+              .fixedSize(horizontal: false, vertical: true)
+              .font(.custom("NotoSerif", size: 15))
+              .bold()
+            Text("Pittsburgh, PA 15217") // TODO: Show this
+              .frame(width: .infinity)
+              .multilineTextAlignment(.leading)
+              .fixedSize(horizontal: false, vertical: true)
+              .font(.custom("NotoSerif", size: 15))
+          }.padding(.top, 10.0)
+          
+          Text("Note:")
+            .frame(width: .infinity)
+            .multilineTextAlignment(.leading)
+            .fixedSize(horizontal: false, vertical: true)
+            .font(.custom("NotoSerif", size: 15))
+            .bold().padding(.top, 10.0)
+          
+          Text(request.note)
+            .multilineTextAlignment(.leading)
+            .fixedSize(horizontal: false, vertical: true)
+            .font(.custom("NotoSerif", size: 15))
           
         }.padding(.horizontal, 20.0)
       }
@@ -55,6 +131,6 @@ struct AlertRequestView: View {
 
 struct AlertRequestView_Previews: PreviewProvider {
   static var previews: some View {
-    AlertRequestView()
+    AlertRequestView(request: Request(userID: 1, userProfile: "https://femindharamshi.com/static/media/favicon.df59357d43584154d3d1.png", userName: "Femin Dharamshi", copyID: 5, listingID: 5, date: "11th Nov, 2022", lat: 45.0, lon: -79.92, note: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin facilisis lacus in vulputate molestie. Sed bibendum malesuada tortor vel pretium. Aliquam sollicitudin ullamcorper sodales. Mauris elit ante, auctor ut enim in, lobortis dapibus justo. Vivamus tristique sem molestie magna aliquam faucibus sodales a est. Proin elementum, sapien sit amet luctus vestibulum, tortor nisi lacinia sapien, ac semper ligula tellus eget tortor. Donec ac elit turpis. In at dolor eu est eleifend blandit. Morbi eget commodo nisi, non porta ipsum. Aenean lobortis diam a elit finibus ultrices. Nulla diam magna, dapibus vel mollis ac, eleifend at lacus. Nullam elementum dolor at auctor scelerisque. Integer posuere erat ac laoreet malesuada.", requestID: 8, coverURL: "https://covers.openlibrary.org/b/id/10290658-L.jpg", title: "Linchpin", listingLOC: ListingLOC(lat: 40.0, lon: -78)))
   }
 }
--- Frontend/CrossMyBook/CrossMyBook/Views/AlertView.swift
+++ Frontend/CrossMyBook/CrossMyBook/Views/AlertView.swift
@@ -44,83 +44,94 @@ struct AlertView: View {
         Text("Requests")
           .font(.custom("NotoSerif", size: 25)).bold().frame(maxWidth: .infinity).foregroundColor(.fontBlack)
         Button (action: {
-          self.presentationMode.wrappedValue.dismiss() // TODO: back action
+          if(currentRequest == nil) {
+            self.presentationMode.wrappedValue.dismiss()
+          } else {
+            currentRequest = nil
+          }
+          
         }) {
           FAIcon(name: "chevron-left", size: 25)
         }.padding(.trailing, 20.0)
       }.padding(10).background(Color(red: 245/255, green: 245 / 255, blue: 245 / 255))
       
-      // MARK: TAB BAR
-      VStack {
-        HStack {
-          
-          if(index == 0)
-          {
-            Text("Pending")
-              .frame(maxWidth: .infinity)
-              .padding(10.0)
-              .font(.custom("NotoSerif", size: 16))
-              .background(Color("Theme"))
-              .foregroundColor(Color.white)
-              .cornerRadius(25.0)
-          } else {
-            Text("Pending")
-              .frame(maxWidth: .infinity)
-              .font(.custom("NotoSerif", size: 16))
-              .padding(10.0)
-              .cornerRadius(25.0)
-              .onTapGesture(perform: toggleIndex)
-          }
-          
-          if(index == 1)
-          {
-            Text("Accepted")
-              .frame(maxWidth: .infinity)
-              .font(.custom("NotoSerif", size: 16))
-              .padding(10.0)
-              .background(Color("Theme"))
-              .foregroundColor(Color.white)
-              .cornerRadius(25.0)
-          } else {
-            Text("Accepted")
-              .frame(maxWidth: .infinity)
-              .font(.custom("NotoSerif", size: 16))
-              .padding(10.0)
-              .cornerRadius(25.0)
-              .onTapGesture(perform: toggleIndex)
-          }
-        }.background(Color.white).cornerRadius(25.0)
-      }.padding(.horizontal, 20.0)
-      
-      ScrollView {
+      if(currentRequest == nil) {
+        // MARK: TAB BAR
         VStack {
-          ForEach(index == 0 ? alertController.observedCopy?.pendingRequests ?? [] : alertController.observedCopy?.acceptedRequests ?? []) { request in
-            HStack (alignment: .top) {
-              VStack {
-                WebImage(url: URL(string: request.coverURL))
-                  .resizable()
-                  .placeholder(Image(uiImage: UIImage(named: "bookplaceholder")!)) // Placeholder Image
-                  .scaledToFit()
-                  .frame(width: 80, height: 120, alignment: .center)
-                  .border(Color.black, width: 1)
-                  .background(Color.brown)
-                  .onTapGesture(perform: {
-                    print("Jump to RequestView")
-                  })
-                Text(request.title).font(.custom("NotoSerif", size: 15)).bold().frame(width: 80).lineLimit(4)
-              }
-              VStack (alignment: .leading) {
-                Text("Distance: \(getDistance(request.lat, request.lon, request.listingLOC.lat, request.listingLOC.lon)) mi.").padding(.bottom, 5.0)
-                Text("Requester:").font(.custom("NotoSerif", size: 16)).bold()
-                Text("\(request.userName)").padding(.bottom, 5.0)
-                Text("Note:").font(.custom("NotoSerif", size: 16)).bold()
-                Text("\(request.note)")
-              }
-              Spacer()
-            }.padding(.horizontal, 20.0)
-            Divider()
+          HStack {
+            
+            if(index == 0)
+            {
+              Text("Pending")
+                .frame(maxWidth: .infinity)
+                .padding(10.0)
+                .font(.custom("NotoSerif", size: 16))
+                .background(Color("Theme"))
+                .foregroundColor(Color.white)
+                .cornerRadius(25.0)
+            } else {
+              Text("Pending")
+                .frame(maxWidth: .infinity)
+                .font(.custom("NotoSerif", size: 16))
+                .padding(10.0)
+                .cornerRadius(25.0)
+                .onTapGesture(perform: toggleIndex)
+            }
+            
+            if(index == 1)
+            {
+              Text("Accepted")
+                .frame(maxWidth: .infinity)
+                .font(.custom("NotoSerif", size: 16))
+                .padding(10.0)
+                .background(Color("Theme"))
+                .foregroundColor(Color.white)
+                .cornerRadius(25.0)
+            } else {
+              Text("Accepted")
+                .frame(maxWidth: .infinity)
+                .font(.custom("NotoSerif", size: 16))
+                .padding(10.0)
+                .cornerRadius(25.0)
+                .onTapGesture(perform: toggleIndex)
+            }
+          }.background(Color.white).cornerRadius(25.0)
+        }.padding(.horizontal, 20.0)
+        
+        ScrollView {
+          VStack {
+            ForEach(index == 0 ? alertController.observedCopy?.pendingRequests ?? [] : alertController.observedCopy?.acceptedRequests ?? []) { request in
+              HStack (alignment: .top) {
+                VStack {
+                  WebImage(url: URL(string: request.coverURL))
+                    .resizable()
+                    .placeholder(Image(uiImage: UIImage(named: "bookplaceholder")!)) // Placeholder Image
+                    .scaledToFit()
+                    .frame(width: 80, height: 120, alignment: .center)
+                    .border(Color.black, width: 1)
+                    .background(Color.brown)
+                    .onTapGesture(perform: {
+                      print("Jump to RequestView")
+                    })
+                  Text(request.title).font(.custom("NotoSerif", size: 15)).bold().frame(width: 80).lineLimit(4)
+                }
+                VStack (alignment: .leading) {
+                  Text("Distance: \(getDistance(request.lat, request.lon, request.listingLOC.lat, request.listingLOC.lon)) mi.").padding(.bottom, 5.0)
+                  Text("Requester:").font(.custom("NotoSerif", size: 16)).bold()
+                  Text("\(request.userName)").padding(.bottom, 5.0)
+                  Text("Note:").font(.custom("NotoSerif", size: 16)).bold()
+                  Text("\(request.note)")
+                }
+                Spacer()
+              }.padding(.horizontal, 20.0).onTapGesture(perform: {
+                currentRequest = request
+              })
+              Divider()
+            }
           }
         }
+      } else {
+        AlertRequestView(request: currentRequest!)
       }
       
       
