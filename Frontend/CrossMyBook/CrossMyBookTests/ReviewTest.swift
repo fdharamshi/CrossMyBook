c1356126afc00d802a8d52db44c22b5c65e7e997
zcj <zcj0709@gmail.com>
2022-12-07 11:27:18 -0500

    user test added

--- Frontend/CrossMyBook/CrossMyBookTests/ReviewTest.swift
+++ Frontend/CrossMyBook/CrossMyBookTests/ReviewTest.swift
@@ -9,28 +9,85 @@ import XCTest
 @testable import CrossMyBook
 
 final class ReviewTest: XCTestCase {
-
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+    
+    let expired: TimeInterval = 10
+    var expectation: XCTestExpectation!
+    let urlString = "http://ec2-3-87-92-147.compute-1.amazonaws.com:8000/"
+    let validUserId = "1"
+    let invalidUserId = "0"
+    let validReviewType = "1"
+    let relatedReviewType = "2"
+    let invalidReviewType = "0"
+    
+    func test_ServerResponse() {
+        defer { waitForExpectations(timeout: expired) }
+        
+        let url = URL(string: urlString + "getReviews?user_id="+validUserId+"&review_type="+validReviewType)!
+        URLSession.shared.dataTask(with: url) { data, response, error in
+            XCTAssertNotNil(data)
+            XCTAssertNotNil(response)
+            XCTAssertNil(error)
+            self.expectation.fulfill()
         }
+        .resume()
     }
+    func test_ServerResponse2() {
+        defer { waitForExpectations(timeout: expired) }
+        
+        let url = URL(string: urlString + "getReviews?user_id="+validUserId+"&review_type="+relatedReviewType)!
+        URLSession.shared.dataTask(with: url) { data, response, error in
+            XCTAssertNotNil(data)
+            XCTAssertNotNil(response)
+            XCTAssertNil(error)
+            self.expectation.fulfill()
+        }
+        .resume()
+    }
+    
+    func test_invalidReviewType() {
+        defer { waitForExpectations(timeout: expired) }
+        
+        let url = URL(string: urlString + "getReviews?user_id="+validUserId+"&review_type="+invalidReviewType)!
+        URLSession.shared.dataTask(with: url) { data, response, error in
+            XCTAssertNotNil(data)
+            XCTAssertNotNil(response)
+            XCTAssertNil(error)
+            if let data = data {
+                do {
+                    let res = try JSONDecoder().decode(Msg.self, from: data)
+                    print(res)
+                    XCTAssertEqual(res.msg, "Invalid review type")
+                    XCTAssertEqual(res.success, false)
+                } catch let error {
+                    print(error)
+                }
+            }
+            self.expectation.fulfill()
+        }.resume()
+    }
+    
+    func test_invalidUserId() {
+        defer { waitForExpectations(timeout: expired) }
+        
+        let url = URL(string: urlString + "getReviews?user_id="+invalidUserId+"&review_type="+validReviewType)!
+        URLSession.shared.dataTask(with: url) { data, response, error in
+            XCTAssertNotNil(data)
+            XCTAssertNotNil(response)
+            XCTAssertNil(error)
+            if let data = data {
+                do {
+                    let res = try JSONDecoder().decode(Msg.self, from: data)
+                    print(res)
+                    XCTAssertEqual(res.msg, "User is not logged in.")
+                    XCTAssertEqual(res.success, false)
+                } catch let error {
+                    print(error)
+                }
+            }
+            self.expectation.fulfill()
+        }.resume()
+    }
+
+    
 
 }
